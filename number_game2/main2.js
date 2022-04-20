31f2aa7a680432cdc2bdf02d28cacc8c1a138af8
liondot <51257576+liondot@users.noreply.github.com>
2022-04-20 16:49:31 +0900

    number game second commit

--- number_game2/main2.js
+++ number_game2/main2.js
@@ -8,11 +8,74 @@
 // 사용자가 1~100범위 밖에 숫자를 입력할 경우 노티스, 기회를 깍지 않음 
 // 사용자가 이미 입력한 숫자를 또 입력할 경우 노티스, 기회를 깍지 않음 
 
-
 let computerNum = 0;
 let playButton = document.getElementById('play-button');
 let userInput = document.getElementById('user-input')
+let resultArea = document.getElementById('result-area')
+let resetButton = document.getElementById('reset-button')
+let chances = 5;
+let gameOver = false;
+let chanceArea = document.getElementById('chance-area')
+let historyNum = []
+
+playButton.addEventListener('click', play)
+resetButton.addEventListener('click', reset)
+userInput.addEventListener('focus', function(){
+  userInput.value = '';
+})
+
+function pickRandomNum() {
+  computerNum = parseInt(Math.random() * 100) +1
+  console.log("정답", computerNum)
+}
 
 function play() {
-  let userValue = userInput.value
+  userValue = userInput.value;
+
+  if(userValue < 1 || userValue > 100) {
+    resultArea.textContent = "1~100사이 숫자를 입력해 주세요"
+    return;
+  }
+
+  if(historyNum.includes(userValue)) {
+    resultArea.textContent = "이미 입력한 값입니다. 다른 숫자를 입력해주세요";
+    return;
+  }
+
+  chances --;
+  chanceArea.textContent = `남은기회 ${chances} 회`
+
+  if(userValue > computerNum) {
+    resultArea.textContent = "Down!!!"
+  } else if (userValue < computerNum) {
+    resultArea.textContent = "Up!!!"
+  } else {
+    resultArea.textContent = "맞추셨습니다!!!"
+    gameOver = true
+  }
+
+  historyNum.push(userValue)
+ 
+
+  if(chances < 1) {
+    gameOver = true;
+  }
+
+  if (gameOver == true) {
+    playButton.disabled = true;
+  }
+
+ 
 }
+
+function reset(){
+  // user input 창에 있는 텍스트가 사라지고 
+  userInput.value = ""
+  // 새로운 컴퓨터 넘버가 생성되고 
+  pickRandomNum()
+
+  resultArea.textContent = "결과값이 여기 나옵니다!"
+
+}
+
+pickRandomNum()
