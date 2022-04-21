9112d49aa2b2a2b2fde0d998ec2e5f4aef9aec0d
yoon <funfunjulia@gmail.com>
2022-04-21 13:51:11 +0900

    gif파일 src

--- number_game2/main2.js
+++ number_game2/main2.js
@@ -12,6 +12,8 @@ let computerNum = 0;
 let playButton = document.getElementById('play-button');
 let userInput = document.getElementById('user-input')
 let resultArea = document.getElementById('result-area')
+let resultAreaImg = document.querySelector('.main-img')
+let resultTxt = document.querySelector('.result-txt')
 let resetButton = document.getElementById('reset-button')
 let chances = 5;
 let gameOver = false;
@@ -20,45 +22,49 @@ let historyNum = []
 
 playButton.addEventListener('click', play)
 resetButton.addEventListener('click', reset)
-userInput.addEventListener('focus', function(){
+userInput.addEventListener('focus', function () {
   userInput.value = '';
 })
 
 function pickRandomNum() {
-  computerNum = parseInt(Math.random() * 100) +1
+  computerNum = parseInt(Math.random() * 100) + 1
   console.log("정답", computerNum)
 }
 
 function play() {
   userValue = userInput.value;
 
-  if(userValue < 1 || userValue > 100) {
+  if (userValue < 1 || userValue > 100) {
     resultArea.textContent = "1~100사이 숫자를 입력해 주세요"
     return;
   }
 
-  if(historyNum.includes(userValue)) {
+  if (historyNum.includes(userValue)) {
     resultArea.textContent = "이미 입력한 값입니다. 다른 숫자를 입력해주세요";
     return;
   }
 
-  chances --;
+  chances--;
   chanceArea.textContent = `남은기회 ${chances} 회`
   historyNum.push(userValue)
 
 
-  if(userValue > computerNum) {
-    resultArea.textContent = "Down!!!"
+  if (userValue > computerNum) {
+    resultAreaImg.src = "https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif";
+    resultTxt.textContent = "Down!!!"
   } else if (userValue < computerNum) {
-    resultArea.textContent = "Up!!!"
+    resultAreaImg.src = "https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif";
+    resultTxt.textContent = "Up!!!"
   } else {
-    resultArea.textContent = "맞추셨습니다!!!"
+    resultAreaImg.src =
+      "https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif";
+    resultTxt.textContent = "맞추셨습니다!!!"
     gameOver = true
   }
 
- 
 
-  if(chances < 1) {
+
+  if (chances < 1) {
     gameOver = true;
   }
 
@@ -66,22 +72,22 @@ function play() {
     playButton.disabled = true;
   }
 
- 
+
 }
 
-function reset(){
+function reset() {
   // user input 창에 있는 텍스트가 사라지고 
   userInput.value = ""
   // 새로운 컴퓨터 넘버가 생성되고 
   pickRandomNum()
-
-  resultArea.textContent = "결과값이 여기 나옵니다!"
+  userInput.value = "";
+  resultAreaImg.src = "https://media1.giphy.com/media/9DinPR8bzFsmf74j9W/giphy.gif";
+  resultTxt.textContent = "죽기 싫다면 맞춰라";
   gameOver = false;
   playButton.disabled = false;
   chances = 5;
   chanceArea.innerHTML = `남은 기회: ${chances}`;
   historyNum = [];
-
 }
 
 pickRandomNum()
